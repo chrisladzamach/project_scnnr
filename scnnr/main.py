be6f1ff0a87d592d9474cb024d8557cb3522cee9
OrichalcosShunnoros <c3146092091@gmail.com>
2025-07-22 00:18:01 -0500

    Uptade in main.py file | code added.

--- scnnr/main.py
+++ scnnr/main.py
@@ -1,1 +1,7 @@
-# This file is the main entry point of the scnnr application
+from ui.menu import main_menu
+
+if __name__ == "__main__":
+    try:
+        main_menu()
+    except KeyboardInterrupt:
+        print("\n\nSaliendo del sistema...")
