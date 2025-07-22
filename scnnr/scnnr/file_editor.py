26e193982a49e7e41645988f109575bc256f3f25
OrichalcosShunnoros <c3146092091@gmail.com>
2025-07-22 01:57:10 -0500

    Update: Code structure added in file_editor.py

--- scnnr/scnnr/file_editor.py
+++ scnnr/scnnr/file_editor.py
@@ -4,9 +4,9 @@ from ui.display import success, warning, error
 
 def modificar_comentario(archivo, linea, nuevo_comentario):
   if not solicitar_contraseña():
-    error("Contraseña incorrecta. No se realiazron cambios.")
+    error("Contraseña incorrecta. No se realizaron cambios.")
     return
-  
+
   try:
     with open(archivo, 'r', encoding='utf-8') as f:
       lineas = f.readlines()
@@ -14,13 +14,13 @@ def modificar_comentario(archivo, linea, nuevo_comentario):
     if linea < 1 or linea > len(lineas):
       error("Número de línea fuera de rango.")
       return
-    
+
     anterior = lineas[linea - 1]
     lineas[linea - 1] = nuevo_comentario + '\n'
 
     with open(archivo, 'w', encoding='utf-8') as f:
-      f.writelines(lines)
-    
+      f.writelines(lineas)
+
     success(f"Comentario en la línea {linea} modificado con éxito.")
     print(f"Antes: {anterior.strip()}\nDespués: {nuevo_comentario}")
   except Exception as e:
@@ -28,25 +28,24 @@ def modificar_comentario(archivo, linea, nuevo_comentario):
 
 def eliminar_comentario(archivo, linea):
   if not solicitar_contraseña():
-    error("Contraseña incorrecta. No se realiazdon cambios.")
+    error("Contraseña incorrecta. No se realizaron cambios.")
     return
-  
+
   try:
     with open(archivo, 'r', encoding='utf-8') as f:
       lineas = f.readlines()
 
     if linea < 1 or linea > len(lineas):
-      error("Número de líneas fuera de rango.")
+      error("Número de línea fuera de rango.")
       return
-    
+
     contenido = lineas[linea - 1].strip()
-    linea[lineas - 1] = '\n'
+    lineas[linea - 1] = '\n'
 
     with open(archivo, 'w', encoding='utf-8') as f:
       f.writelines(lineas)
 
-    succes(f"Comentario en la línea {linea} eliminado.")
+    success(f"Comentario en la línea {linea} eliminado.")
     print(f"Contenido eliminado: {contenido}")
   except Exception as e:
     error(f"Error eliminando comentario: {str(e)}")
-
